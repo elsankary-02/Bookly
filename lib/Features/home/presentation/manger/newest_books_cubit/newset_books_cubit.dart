301d076c2649dd8274d355df8846ae6f4dbfd039
elsankary-02 <githup800900@gmail.com>
2025-01-08 14:42:15 +0200

    change

--- lib/Features/home/presentation/manger/newest_books_cubit/newset_books_cubit.dart
+++ lib/Features/home/presentation/manger/newest_books_cubit/newset_books_cubit.dart
@@ -1,7 +1,7 @@
-import 'package:bloc/bloc.dart';
-import '../../../data/models/book_model/book_model.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../data/models/book_model/book_model.dart';
 import '../../../data/repos/home_repo.dart';
 
 part 'newset_books_state.dart';
